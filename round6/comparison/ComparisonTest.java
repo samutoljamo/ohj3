4468c0c7c28347470896322c12db5739da7b0f16
Jorma Laurikkala <jorma.laurikkala@tuni.fi>
2023-02-06 11:48:21 +0200

    ComparisonTest updated.

--- round6/comparison/ComparisonTest.java
+++ round6/comparison/ComparisonTest.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class ComparisonTest {
+  // Quick hack by Jorma L. to avoid a non-standard toString override in Attainment.
+  private static void print(String header, ArrayList<Attainment> attainments) {
+     System.out.println(header);
+     String asLines = attainments.toString().replace(",", "\n,");
+     asLines = asLines.replace("]", "\n]");
+     System.out.println(asLines);
+  }     
+  
   public static void main(String[] args) throws IOException {
     String attainmentFN = args[0];
     ArrayList<Attainment> attainments = new ArrayList<>();
@@ -16,13 +24,13 @@ public class ComparisonTest {
                 studentCourseGrade[0], Integer.parseInt(studentCourseGrade[2])));
       }
     }
-    System.out.format("Original order:%n%s%n", attainments);
+    print("Original order:", attainments);
     Collections.sort(attainments);
-    System.out.format("Sorted in default order:%n%s%n", attainments);
+    print("Sorted in default order:", attainments);
     attainments.sort(Attainment.CODE_GRADE_CMP);
-    System.out.format("Sorted using CODE_GRADE_CMP:%n%s%n", attainments);
+    print("Sorted using CODE_GRADE_CMP:", attainments);
     attainments.sort(Attainment.CODE_STUDENT_CMP);
-    System.out.format("Sorted using CODE_STUDENT_CMP:%n%s%n", attainments);
+    print("Sorted using CODE_STUDENT_CMP:", attainments);
   }
 
 }
